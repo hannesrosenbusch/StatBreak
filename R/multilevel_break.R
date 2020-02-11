c455686ec5288dcdd2f0a54c99b0c9a3364e0956
Hannes Rosenbusch <hannes.rosenbusch@gmail.com>
2020-02-11 16:02:59 +0100

    multilevel

--- R/multilevel_break.R
+++ R/multilevel_break.R
@@ -376,6 +376,8 @@ multilevel_break = function(data = NULL,#a data.frame containing the observation
   solution = best(alg)
   excluded_groups = unique(data[,grouping_var])[solution == 1]
   nr_excluded_groups = length(excluded_groups)
-  output = list("excluded groups" = excluded_groups, "number_exclusions"= nr_excluded_groups)
+  x = statistic_computation(data[is.element(data[,grouping_var],-excluded_groups),])
+  output = list("excluded groups" = excluded_groups, "number_exclusions"= nr_excluded_groups, "new_value" = x)
+
   return(output)
 }
